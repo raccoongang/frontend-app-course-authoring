f9bc80a2b1385b82017780562344a56093a1e2fb
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-06-19 18:53:35 +0300

    refactor: refactoring resize logic

--- src/grading-settings/GradingScale.jsx
+++ src/grading-settings/GradingScale.jsx
@@ -10,22 +10,53 @@ const GradingScale = () => {
   ]);
 
   const addNewGradingSegment = () => {
-    setGradingSegments(prevValues => [
-      ...prevValues,
-      {
-        current: prevValues[prevValues.length - 1].current + 15,
-        previous: prevValues[prevValues.length - 1].current,
-      },
-    ]);
+    setGradingSegments(prevValues => {
+      const first = prevValues[prevValues.length - 1];
+      const second = prevValues[prevValues.length - 2];
+
+      const newCurrent = Math.ceil((second.current - second.previous) / 2);
+
+      const newValue = {
+        current: first.current + newCurrent,
+        previous: first.current,
+      };
+
+      const updatedSecond = {
+        ...second,
+        previous: first.current + newCurrent,
+      };
+
+      return [
+        ...prevValues.slice(0, prevValues.length - 2),
+        updatedSecond,
+        newValue,
+        first,
+      ];
+    });
   };
 
-  const updateGradingSegments = (newGradingSegmentData) => {
-    setGradingSegments(prevValues => newGradingSegmentData.sort().map((newGradingSegmentValue, i) => ({
-      current: newGradingSegmentValue,
-      // eslint-disable-next-line no-nested-ternary
-      previous: i === 0
-        ? prevValues[newGradingSegmentData.length - 1].current : i === 1 ? 0 : prevValues[i - 1].current,
-    })));
+  const updateGradingSegments = (newGradingSegmentData, activeHandleIndex) => {
+    const sortedSegments = newGradingSegmentData.sort((a, b) => a - b);
+    const newValue = sortedSegments[sortedSegments.length - 1 - activeHandleIndex];
+    const prevSegmentBoundary = (gradingSegments[activeHandleIndex + 1]
+        && gradingSegments[activeHandleIndex + 1].current) || 0;
+    const nextSegmentBoundary = gradingSegments[activeHandleIndex - 1].current;
+
+    setGradingSegments(gradingSegments.map((segment, idx) => {
+      const upperBoundaryValue = newValue < nextSegmentBoundary - 5 ? newValue : nextSegmentBoundary - 5;
+      const lowerBoundaryValue = upperBoundaryValue > prevSegmentBoundary + 5
+        ? upperBoundaryValue : prevSegmentBoundary + 5;
+
+      if (idx === activeHandleIndex - 1) {
+        return { previous: lowerBoundaryValue, current: segment.current };
+      }
+
+      if (idx === activeHandleIndex) {
+        return { current: lowerBoundaryValue, previous: segment.previous };
+      }
+
+      return segment;
+    }));
   };
 
   const handleRemoveGradingSegment = (gradingSegmentIndex) => {
@@ -33,19 +64,18 @@ const GradingScale = () => {
       .filter((_, i) => i !== gradingSegmentIndex));
   };
 
-  const setGradingSegmentsValues = () => gradingSegments.map(segment => segment.current);
-
   const {
     getTrackProps,
     ticks,
     segments,
     handles,
+    activeHandleIndex,
   } = useRanger({
     min: 0,
     max: 100,
     stepSize: 1,
-    values: setGradingSegmentsValues(),
-    onDrag: updateGradingSegments,
+    values: gradingSegments.map(segment => segment.current),
+    onDrag: (segmentDataArray) => updateGradingSegments(segmentDataArray, activeHandleIndex),
   });
 
   return (
