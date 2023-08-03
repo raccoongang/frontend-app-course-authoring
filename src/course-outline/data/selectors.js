529d3942d9ef23947979049252c8c6979c7cde2a
Vladislav Keblysh <vladislavkeblysh@Vladislavs-MacBook-Pro.local>
2023-08-03 14:05:08 +0300

    feat: [2u-259] remove warning from selectors

--- src/course-outline/data/selectors.js
+++ src/course-outline/data/selectors.js
@@ -1,3 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export const getOutlineIndexData = (state) => state.courseOutline.outlineIndexData;
 export const getLoadingOutlineIndexStatus = (state) => state.courseOutline.loadingOutlineIndexStatus;
