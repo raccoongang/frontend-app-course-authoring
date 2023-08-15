4aedae63a0ecc2d40b36642d6da9b88d16ecbf84
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2023-08-15 12:47:24 +0300

    refactor: [2U-161] add help-urls

--- src/export-page/export-sidebar/ExportSidebar.jsx
+++ src/export-page/export-sidebar/ExportSidebar.jsx
@@ -5,37 +5,40 @@ import {
 } from '@edx/frontend-platform/i18n';
 import PropTypes from 'prop-types';
 import { Button } from '@edx/paragon';
-import { getConfig } from '@edx/frontend-platform';
 
 import HelpSidebar from '../../generic/help-sidebar';
 import messages from './messages';
+import { useHelpUrls } from '../../help-urls/hooks';
 
-const ExportSidebar = ({ intl, courseId }) => (
-  <HelpSidebar courseId={courseId}>
-    <h4 className="help-sidebar-about-title">{intl.formatMessage(messages.title1)}</h4>
-    <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.description1)}</p>
-    <hr />
-    <h4 className="help-sidebar-about-title">{intl.formatMessage(messages.exportedContent)}</h4>
-    <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.exportedContentHeading)}</p>
-    <ul className="export-sidebar-list">
-      <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content1)}</li>
-      <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content2)}</li>
-      <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content3)}</li>
-      <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content4)}</li>
-      <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content5)}</li>
-    </ul>
-    <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.notExportedContent)}</p>
-    <ul className="export-sidebar-list">
-      <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content6)}</li>
-      <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content7)}</li>
-    </ul>
-    <hr />
-    <h4 className="help-sidebar-about-title">{intl.formatMessage(messages.openDownloadFile)}</h4>
-    <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.openDownloadFileDescription)}</p>
-    <hr />
-    <Button href={getConfig().EXPORT_LEARN_MORE_URL} target="_blank" variant="outline-primary">{intl.formatMessage(messages.learnMoreButtonTitle)}</Button>
-  </HelpSidebar>
-);
+const ExportSidebar = ({ intl, courseId }) => {
+  const { docUrl: exportLearnMoreUrl } = useHelpUrls(['doc_url']);
+  return (
+    <HelpSidebar courseId={courseId}>
+      <h4 className="help-sidebar-about-title">{intl.formatMessage(messages.title1)}</h4>
+      <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.description1)}</p>
+      <hr />
+      <h4 className="help-sidebar-about-title">{intl.formatMessage(messages.exportedContent)}</h4>
+      <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.exportedContentHeading)}</p>
+      <ul className="export-sidebar-list">
+        <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content1)}</li>
+        <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content2)}</li>
+        <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content3)}</li>
+        <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content4)}</li>
+        <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content5)}</li>
+      </ul>
+      <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.notExportedContent)}</p>
+      <ul className="export-sidebar-list">
+        <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content6)}</li>
+        <li className="help-sidebar-about-descriptions">{intl.formatMessage(messages.content7)}</li>
+      </ul>
+      <hr />
+      <h4 className="help-sidebar-about-title">{intl.formatMessage(messages.openDownloadFile)}</h4>
+      <p className="help-sidebar-about-descriptions">{intl.formatMessage(messages.openDownloadFileDescription)}</p>
+      <hr />
+      <Button href={exportLearnMoreUrl} target="_blank" variant="outline-primary">{intl.formatMessage(messages.learnMoreButtonTitle)}</Button>
+    </HelpSidebar>
+  );
+};
 
 ExportSidebar.propTypes = {
   intl: intlShape.isRequired,
