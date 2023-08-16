7e6aac41716057a4d10509cbe49a1104df5997fa
Kyrylo Kholodenko <kyrylo.kholodenko@gmail.com>
2023-08-16 13:46:26 +0300

    feat: implement export page (#32)
    * feat: implement export page
    
    * feat: [2u-247] create CourseStepper
    
    * feat: implement export page
    
    * feat: add tests for export page
    
    * feat: add internet connection alert
    
    * refactor: [2U-161] fix imports and remove unused elements
    
    * refactor: [2U-161] add help-urls
    
    * refactor: [2U-161] refactor code according to review feedback
    
    ---------
    
    Co-authored-by: Vladislav Keblysh <vladislavkeblysh@Vladislavs-MacBook-Pro.local>

--- src/store.js
+++ src/store.js
@@ -15,6 +15,7 @@ import { reducer as CourseUpdatesReducer } from './course-updates/data/slice';
 import { reducer as courseOutlineReducer } from './course-outline/data/slice';
 import { reducer as genericReducer } from './generic/data/slice';
 import { reducer as helpUrlsReducer } from './help-urls/data/slice';
+import { reducer as courseExportReducer } from './export-page/data/slice';
 
 export default function initializeStore(preloadedState = undefined) {
   return configureStore({
@@ -34,6 +35,7 @@ export default function initializeStore(preloadedState = undefined) {
       courseOutline: courseOutlineReducer,
       generic: genericReducer,
       helpUrls: helpUrlsReducer,
+      courseExport: courseExportReducer,
     },
     preloadedState,
   });
