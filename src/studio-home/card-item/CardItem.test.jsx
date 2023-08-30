32b3d95d30ab6b8a54a99db149c35339c755c205
ruzniaievdm <ruzniaievdm@gmail.com>
2023-08-30 13:57:26 +0300

    feat: made the courses as read-only if old mongo store

--- src/studio-home/card-item/CardItem.test.jsx
+++ src/studio-home/card-item/CardItem.test.jsx
@@ -16,9 +16,8 @@ jest.mock('react-redux', () => ({
 }));
 
 let store;
-const props = studioHomeMock.archivedCourses[0];
 
-const RootWrapper = () => (
+const RootWrapper = (props) => (
   <AppProvider store={store}>
     <IntlProvider locale="en" messages={{}}>
       <CardItem intl={{ formatMessage: jest.fn() }} {...props} />
@@ -40,11 +39,13 @@ describe('<CardItem />', () => {
     useSelector.mockReturnValue(studioHomeMock);
   });
   it('should render course details for non-library course', () => {
-    const { getByText } = render(<RootWrapper />);
+    const props = studioHomeMock.archivedCourses[0];
+    const { getByText } = render(<RootWrapper {...props} />);
     expect(getByText(`${props.org} / ${props.number} / ${props.run}`)).toBeInTheDocument();
   });
   it('should render correct links for non-library course', () => {
-    const { getByText } = render(<RootWrapper />);
+    const props = studioHomeMock.archivedCourses[0];
+    const { getByText } = render(<RootWrapper {...props} />);
     const courseTitleLink = getByText(props.displayName);
     expect(courseTitleLink).toHaveAttribute('href', `${getConfig().STUDIO_BASE_URL}${props.url}`);
     const btnReRunCourse = getByText(messages.btnReRunText.defaultMessage);
@@ -53,15 +54,23 @@ describe('<CardItem />', () => {
     expect(viewLiveLink).toHaveAttribute('href', props.lmsLink);
   });
   it('should render course details for library course', () => {
-    props.isLibraries = true;
-    const { getByText } = render(<RootWrapper />);
+    const props = { ...studioHomeMock.archivedCourses[0], isLibraries: true };
+    const { getByText } = render(<RootWrapper {...props} />);
     const courseTitleLink = getByText(props.displayName);
     expect(courseTitleLink).toHaveAttribute('href', `${getConfig().STUDIO_BASE_URL}${props.url}`);
     expect(getByText(`${props.org} / ${props.number}`)).toBeInTheDocument();
   });
   it('should hide rerun button if disallowed', () => {
+    const props = studioHomeMock.archivedCourses[0];
     useSelector.mockReturnValue({ ...studioHomeMock, allowCourseReruns: false });
-    const { queryByText } = render(<RootWrapper />);
+    const { queryByText } = render(<RootWrapper {...props} />);
     expect(queryByText(messages.btnReRunText.defaultMessage)).not.toBeInTheDocument();
+  });
+  it('should be read only course if old mongo course', () => {
+    const props = studioHomeMock.courses[1];
+    const { queryByText } = render(<RootWrapper {...props} />);
+    expect(queryByText(props.displayName)).not.toHaveAttribute('href');
+    expect(queryByText(messages.btnReRunText.defaultMessage)).not.toBeInTheDocument();
+    expect(queryByText(messages.viewLiveBtnText.defaultMessage)).not.toBeInTheDocument();
   });
 });
