3aa7257f38f80e5aeb61c06323d86903f3b44155
monteri <lansevermore>
2024-01-05 18:47:47 +0200

    feat: [AXIMST-11] add functionality and tests for unit page header

---
+++ src/course-unit/breadcrumbs/messages.js
@@ -0,0 +1,10 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  altIconChevron: {
+    id: 'course-authoring.course-unit.heading.icon.chevron.alt',
+    defaultMessage: 'Toggle dropdown menu',
+  },
+});
+
+export default messages;
