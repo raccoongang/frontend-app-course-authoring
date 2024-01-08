0986b7472dc70fc1e6bcdf6cad6ffa701ccddb1f
Ihor Romaniuk <notsuchasall25@gmail.com>
2024-01-08 16:30:19 +0100

    feat: [AXIMST-38] Unit page - update breadcrumbs (#92)

--- src/course-unit/breadcrumbs/messages.js
+++ src/course-unit/breadcrumbs/messages.js
@@ -5,6 +5,11 @@ const messages = defineMessages({
     id: 'course-authoring.course-unit.heading.icon.chevron.alt',
     defaultMessage: 'Toggle dropdown menu',
   },
+  loading: {
+    id: 'course-authoring.course-unit.heading.breadcrumbs.loading',
+    defaultMessage: 'Loading...',
+    description: 'Message displayed while loading breadcrumbs content',
+  },
 });
 
 export default messages;
