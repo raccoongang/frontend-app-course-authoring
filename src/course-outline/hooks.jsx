afd462602e3b0973b5b7aebddb2b0c69dfff9f82
Ihor Romaniuk <notsuchasall25@gmail.com>
2024-01-08 16:30:19 +0100

    feat: [AXIMST-38] Unit page - update breadcrumbs (#92)

--- src/course-outline/hooks.jsx
+++ src/course-outline/hooks.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import { useToggle } from '@edx/paragon';
 
 import { RequestStatus } from '../data/constants';
@@ -211,5 +212,24 @@ const useCourseOutline = ({ courseId }) => {
   };
 };
 
-// eslint-disable-next-line import/prefer-default-export
-export { useCourseOutline };
+const useScrollToLocatorElement = ({ isLoading }) => {
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const locator = searchParams.get('show');
+    if (!locator) {
+      return;
+    }
+
+    const locatorToShow = document.querySelector(`[data-locator="${locator}"]`);
+    if (!locatorToShow) {
+      return;
+    }
+    locatorToShow.scrollIntoView({ behavior: 'smooth' });
+  }, [isLoading]);
+};
+
+export {
+  useCourseOutline,
+  useScrollToLocatorElement,
+};
