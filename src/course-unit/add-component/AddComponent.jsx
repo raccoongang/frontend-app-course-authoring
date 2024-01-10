1eb2f5ad7456ed4dc4b6e1245ca707d33fd0fcc9
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2024-01-11 01:25:08 +0200

    feat: added Advanced component list modal window

--- src/course-unit/add-component/AddComponent.jsx
+++ src/course-unit/add-component/AddComponent.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { useIntl } from '@edx/frontend-platform/i18n';
-import { Button } from '@edx/paragon';
+import { Button, useToggle } from '@edx/paragon';
 
 import { getCourseSectionVertical } from '../data/selectors';
 import { COMPONENT_ICON_TYPES } from '../constants';
 import ComponentIcon from './ComponentIcon';
 import messages from './messages';
+import { AdvancedModal } from './modals';
 
 const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
   const intl = useIntl();
+  const [isOpen, open, close] = useToggle(false);
   const { componentTemplates } = useSelector(getCourseSectionVertical);
 
   const handleCreateNewXblock = (type) => () => {
@@ -17,6 +19,12 @@ const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
     case COMPONENT_ICON_TYPES.discussion:
     case COMPONENT_ICON_TYPES.dragAndDrop:
       handleCreateNewCourseXblock({ type, parentLocator: blockId });
+      break;
+    case COMPONENT_ICON_TYPES.library:
+      handleCreateNewCourseXblock({ type, category: 'library_content', parentLocator: blockId });
+      break;
+    case COMPONENT_ICON_TYPES.advanced:
+      handleCreateNewCourseXblock({ type, category: 'advanced', parentLocator: blockId });
       break;
     default:
     }
@@ -30,20 +38,38 @@ const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
     <div className="py-4">
       <h5 className="h3 mb-4 text-center">{intl.formatMessage(messages.title)}</h5>
       <ul className="new-component-type list-unstyled m-0 d-flex flex-wrap justify-content-center">
-        {Object.keys(componentTemplates).map((component) => (
-          <li key={componentTemplates[component].type}>
-            <Button
-              variant="outline-primary"
-              className="add-component-button flex-column rounded-sm"
-              onClick={handleCreateNewXblock(componentTemplates[component].type)}
-            >
-              <ComponentIcon type={componentTemplates[component].type} />
-              <span className="sr-only">{intl.formatMessage(messages.buttonText)}</span>
-              <span className="small mt-2">{componentTemplates[component].displayName}</span>
-            </Button>
-          </li>
-        ))}
+        {Object.keys(componentTemplates).map((component) => {
+          if (componentTemplates[component].type === 'advanced') {
+            return (
+              <li key={componentTemplates[component].type}>
+                <Button
+                  variant="outline-primary"
+                  className="add-component-button flex-column rounded-sm"
+                  onClick={open}
+                >
+                  <ComponentIcon type={componentTemplates[component].type} />
+                  <span className="sr-only">{intl.formatMessage(messages.buttonText)}</span>
+                  <span className="small mt-2">{componentTemplates[component].displayName}</span>
+                </Button>
+              </li>
+            );
+          }
+          return (
+            <li key={componentTemplates[component].type}>
+              <Button
+                variant="outline-primary"
+                className="add-component-button flex-column rounded-sm"
+                onClick={handleCreateNewXblock(componentTemplates[component].type)}
+              >
+                <ComponentIcon type={componentTemplates[component].type} />
+                <span className="sr-only">{intl.formatMessage(messages.buttonText)}</span>
+                <span className="small mt-2">{componentTemplates[component].displayName}</span>
+              </Button>
+            </li>
+          );
+        })}
       </ul>
+      <AdvancedModal isOpen={isOpen} close={close} />
     </div>
   );
 };
