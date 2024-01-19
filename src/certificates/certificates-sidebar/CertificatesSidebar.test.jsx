36d3ab2b69372c46737ccf3f18a987a244ef4270
Kyr <40792129+khudym@users.noreply.github.com>
2024-01-19 19:24:17 +0200

    feat: [AXIMST-154] Certificates page - initial Certificates screen, sidebar (#119)
    * feat: [AXIMST-154] initial Certificates screen, sidebar
    
    * refactor: [AXIMST-154] add changes after review
    
    * refactor: [AXIMST-154] add changes after review
    
    ---------
    
    Co-authored-by: Kyrylo Hudym-Levkovych <kyr.hudym@kyrs-MacBook-Pro.local>

---
+++ src/certificates/certificates-sidebar/CertificatesSidebar.test.jsx
@@ -0,0 +1,56 @@
+import { render, waitFor } from '@testing-library/react';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
+import { initializeMockApp } from '@edx/frontend-platform';
+import { AppProvider } from '@edx/frontend-platform/react';
+
+import initializeStore from '../../store';
+import CertificatesSidebar from '.';
+import messages from './messages';
+
+const courseId = 'course-123';
+let store;
+
+jest.mock('@edx/frontend-platform/i18n', () => ({
+  ...jest.requireActual('@edx/frontend-platform/i18n'),
+  useIntl: () => ({
+    formatMessage: (message) => message.defaultMessage,
+  }),
+}));
+
+const renderComponent = (props) => render(
+  <AppProvider store={store} messages={{}}>
+    <IntlProvider locale="en">
+      <CertificatesSidebar courseId={courseId} {...props} />
+    </IntlProvider>
+  </AppProvider>,
+);
+
+describe('CertificatesSidebar', () => {
+  beforeEach(() => {
+    initializeMockApp({
+      authenticatedUser: {
+        userId: 3,
+        username: 'abc123',
+        administrator: true,
+        roles: [],
+      },
+    });
+    store = initializeStore();
+  });
+
+  it('renders correctly', async () => {
+    const { getByText } = renderComponent();
+    await waitFor(() => {
+      expect(getByText(messages.about.defaultMessage)).toBeInTheDocument();
+      expect(getByText(messages.aboutDescription_1.defaultMessage)).toBeInTheDocument();
+      expect(getByText(messages.aboutDescription_2.defaultMessage)).toBeInTheDocument();
+      expect(getByText(messages.aboutDescription_3.defaultMessage)).toBeInTheDocument();
+
+      expect(getByText(messages.about_2.defaultMessage)).toBeInTheDocument();
+      expect(getByText(messages.about_2_Description_1.defaultMessage)).toBeInTheDocument();
+      expect(getByText(messages.about_2_Description_2.defaultMessage)).toBeInTheDocument();
+
+      expect(getByText(messages.learnMoreBtn.defaultMessage)).toBeInTheDocument();
+    });
+  });
+});
