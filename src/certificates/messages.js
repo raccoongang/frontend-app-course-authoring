5c10a91ef97590c22ec548b1ba6147c31083522d
Kyr <40792129+khudym@users.noreply.github.com>
2024-01-19 19:24:17 +0200

    feat: [AXIMST-154] Certificates page - initial Certificates screen, sidebar (#119)
    * feat: [AXIMST-154] initial Certificates screen, sidebar
    
    * refactor: [AXIMST-154] add changes after review
    
    * refactor: [AXIMST-154] add changes after review
    
    ---------
    
    Co-authored-by: Kyrylo Hudym-Levkovych <kyr.hudym@kyrs-MacBook-Pro.local>

---
+++ src/certificates/messages.js
@@ -0,0 +1,38 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  headingTitle: {
+    id: 'course-authoring.certificates.heading.title',
+    defaultMessage: 'Certificates',
+  },
+  headingSubtitle: {
+    id: 'course-authoring.certificates.heading.subtitle',
+    defaultMessage: 'Settings',
+  },
+  headingActionsPreview: {
+    id: 'course-authoring.certificates.heading.action.button.preview',
+    defaultMessage: 'Preview certificate',
+  },
+  headingActionsDeactivate: {
+    id: 'course-authoring.certificates.heading.action.button.deactivate',
+    defaultMessage: 'Deactivate',
+  },
+  noCertificatesText: {
+    id: 'course-authoring.certificates.nocertificate.text',
+    defaultMessage: 'You haven\'t added any certificates to this course yet.',
+  },
+  setupCertificateBtn: {
+    id: 'course-authoring.certificates.setup.certificate.button',
+    defaultMessage: 'Add your first certificate',
+  },
+  setupCertificateBtnAlt: {
+    id: 'course-authoring.certificates.setup.certificate.button.alt',
+    defaultMessage: 'Add your first certificate',
+  },
+  withoutModesText: {
+    id: 'course-authoring.certificates.without.modes.text',
+    defaultMessage: 'This course does not use a mode that offers certificates.',
+  },
+});
+
+export default messages;
