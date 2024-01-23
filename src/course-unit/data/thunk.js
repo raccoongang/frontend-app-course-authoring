9e139a5a712514b2df456c39c2de77dcd46fbe00
Ihor Romaniuk <notsuchasall25@gmail.com>
2024-01-23 14:17:15 +0100

    feat: [AXIMST-27] Unit page - display xblock components (#122)
    * feat: [AXIMST-27] Unit page - display xblock components
    
    * fix: [AXIMST-27] fix after review
    
    * fix: after review

--- src/course-unit/data/thunk.js
+++ src/course-unit/data/thunk.js
@@ -17,6 +17,7 @@ import {
   getCourseHomeCourseMetadata,
   getCourseSectionVerticalData,
   createCourseXblock,
+  getCourseVerticalChildren,
 } from './api';
 import {
   updateLoadingCourseUnitStatus,
@@ -32,6 +33,8 @@ import {
   fetchCourseSectionVerticalDataSuccess,
   updateLoadingCourseSectionVerticalDataStatus,
   updateLoadingCourseXblockStatus,
+  updateCourseVerticalChildren,
+  updateCourseVerticalChildrenLoadingStatus,
 } from './slice';
 
 export function fetchCourseUnitQuery(courseId) {
@@ -192,7 +195,7 @@ export function fetchCourse(courseId) {
   };
 }
 
-export function createNewCourseXblock(body, callback) {
+export function createNewCourseXBlock(body, callback, blockId) {
   return async (dispatch) => {
     dispatch(updateLoadingCourseXblockStatus({ status: RequestStatus.IN_PROGRESS }));
     dispatch(showProcessingNotification(NOTIFICATION_MESSAGES.adding));
@@ -205,7 +208,8 @@ export function createNewCourseXblock(body, callback) {
             const courseSectionVerticalData = await getCourseSectionVerticalData(result.locator);
             dispatch(fetchCourseSectionVerticalDataSuccess(courseSectionVerticalData));
           }
-          // ToDo: implement fetching (update) xblocks after success creating
+          const courseVerticalChildrenData = await getCourseVerticalChildren(blockId);
+          dispatch(updateCourseVerticalChildren(courseVerticalChildrenData));
           dispatch(hideProcessingNotification());
           dispatch(updateLoadingCourseXblockStatus({ status: RequestStatus.SUCCESSFUL }));
           dispatch(updateSavingStatus({ status: RequestStatus.SUCCESSFUL }));
@@ -218,6 +222,20 @@ export function createNewCourseXblock(body, callback) {
       dispatch(hideProcessingNotification());
       dispatch(updateLoadingCourseXblockStatus({ status: RequestStatus.FAILED }));
       dispatch(updateSavingStatus({ status: RequestStatus.FAILED }));
+    }
+  };
+}
+
+export function fetchCourseVerticalChildrenData(itemId) {
+  return async (dispatch) => {
+    dispatch(updateCourseVerticalChildrenLoadingStatus({ status: RequestStatus.IN_PROGRESS }));
+
+    try {
+      const courseVerticalChildrenData = await getCourseVerticalChildren(itemId);
+      dispatch(updateCourseVerticalChildren(courseVerticalChildrenData));
+      dispatch(updateCourseVerticalChildrenLoadingStatus({ status: RequestStatus.SUCCESSFUL }));
+    } catch (error) {
+      dispatch(updateCourseVerticalChildrenLoadingStatus({ status: RequestStatus.FAILED }));
     }
   };
 }
