14cc409ba6c125d7e5fa6b5ff6cc14748f323ebb
Kyrylo Hudym-Levkovych <kyr.hudym@kyrs-MacBook-Pro.local>
2024-02-12 15:16:15 +0200

    feat: [AXIMST-184,190,196] Certificates page - page preview, verified mode, activate/deactivate

---
+++ src/certificates/layout/header-buttons/hooks/useHeaderButtons.jsx
@@ -0,0 +1,48 @@
+import { useState, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import {
+  getCourseModes, getCertificateActivationUrl, getCertificateWebViewUrl, getIsCertificateActive,
+} from '../../../data/selectors';
+import { updateActivationCertificate } from '../../../data/thunks';
+
+const useHeaderButtons = () => {
+  const { courseId } = useParams();
+  const dispatch = useDispatch();
+  const courseModes = useSelector(getCourseModes);
+  const certificateWebViewUrl = useSelector(getCertificateWebViewUrl);
+  const certificateActivationHandlerUrl = useSelector(getCertificateActivationUrl);
+  const isCertificateActive = useSelector(getIsCertificateActive);
+
+  const [dropdowmItem, setDropdowmItem] = useState(courseModes[0]);
+
+  const handleActivationStatus = () => {
+    const status = !isCertificateActive;
+
+    dispatch(updateActivationCertificate(courseId, certificateActivationHandlerUrl, status));
+  };
+
+  const previewUrl = useMemo(() => {
+    if (!certificateWebViewUrl) { return ''; }
+
+    const url = new URL(certificateWebViewUrl, window.location.origin);
+
+    const searchParams = new URLSearchParams(url.search);
+    searchParams.set('preview', dropdowmItem);
+    url.search = searchParams.toString();
+
+    return url.toString();
+  }, [certificateWebViewUrl, dropdowmItem]);
+
+  return {
+    previewUrl,
+    courseModes,
+    dropdowmItem,
+    isCertificateActive,
+    setDropdowmItem,
+    handleActivationStatus,
+  };
+};
+
+export default useHeaderButtons;
