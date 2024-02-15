cfc0527af8967de5f97b123d62a6f4e526b33ad5
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2024-02-15 17:13:54 +0200

    feat: [AXIMST-46] Course unit - Drag And Drop For Xblocks (#158)
    * feat: [AXIMST-46] Drag And Drop For Xblocks
    
    * feat: added tests
    
    * refactor: refactoring after review

--- src/course-unit/CourseUnit.test.jsx
+++ src/course-unit/CourseUnit.test.jsx
@@ -52,6 +52,7 @@ import { PUBLISH_TYPES, UNIT_VISIBILITY_STATES } from './constants';
 import CourseUnit from './CourseUnit';
 import messages from './messages';
 import configureModalMessages from '../generic/configure-modal/messages';
+import { RequestStatus } from '../data/constants';
 
 let axiosMock;
 let store;
@@ -1412,6 +1413,58 @@ describe('<CourseUnit />', () => {
       userEvent.click(within(errorFilesAlert).getByText(/Dismiss/i));
 
       expect(queryByTestId('has-error-files')).toBeNull();
+    });
+  });
+
+  describe('Drag and drop', () => {
+    it('checks xblock list is restored to original order when API call fails', async () => {
+      const { findAllByRole } = render(<RootWrapper />);
+
+      const xBlocksDraggers = await findAllByRole('button', { name: 'Drag to reorder' });
+      const draggableButton = xBlocksDraggers[1];
+
+      axiosMock
+        .onPut(getXBlockBaseApiUrl(blockId))
+        .reply(500, { dummy: 'value' });
+
+      const xBlock1 = store.getState().courseUnit.courseVerticalChildren.children[0].id;
+
+      fireEvent.keyDown(draggableButton, { key: 'ArrowUp' });
+
+      await waitFor(async () => {
+        fireEvent.keyDown(draggableButton, { code: 'Space' });
+
+        const saveStatus = store.getState().courseUnit.savingStatus;
+        expect(saveStatus).toEqual(RequestStatus.FAILED);
+      });
+
+      const xBlock1New = store.getState().courseUnit.courseVerticalChildren.children[0].id;
+      expect(xBlock1).toBe(xBlock1New);
+    });
+
+    it('check that new xblock list is saved when dragged', async () => {
+      const { findAllByRole } = render(<RootWrapper />);
+
+      const xBlocksDraggers = await findAllByRole('button', { name: 'Drag to reorder' });
+      const draggableButton = xBlocksDraggers[1];
+
+      axiosMock
+        .onPut(getXBlockBaseApiUrl(blockId))
+        .reply(200, { dummy: 'value' });
+
+      const xBlock1 = store.getState().courseUnit.courseVerticalChildren.children[0].id;
+
+      fireEvent.keyDown(draggableButton, { key: 'ArrowUp' });
+
+      await waitFor(async () => {
+        fireEvent.keyDown(draggableButton, { code: 'Space' });
+
+        const saveStatus = store.getState().courseUnit.savingStatus;
+        expect(saveStatus).toEqual(RequestStatus.SUCCESSFUL);
+      });
+
+      const xBlock2 = store.getState().courseUnit.courseVerticalChildren.children[1].id;
+      expect(xBlock1).toBe(xBlock2);
     });
   });
 });
