d55d386c5b24a3aec65523a4e854a22a82858e88
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2024-03-04 15:57:15 +0200

    fix: [AXIMST-558] Course unit - Fixed loading errors and PropTypes (#199)
    * fix: [AXIMST-558] fixed Course unit loading
    
    * refactor: changed selector name

--- src/course-unit/data/selectors.js
+++ src/course-unit/data/selectors.js
@@ -1,12 +1,20 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { RequestStatus } from 'CourseAuthoring/data/constants';
+
 export const getCourseUnitData = (state) => state.courseUnit.unit;
 export const getCanEdit = (state) => state.courseUnit.canEdit;
 export const getStaticFileNotices = (state) => state.courseUnit.staticFileNotices;
 export const getSavingStatus = (state) => state.courseUnit.savingStatus;
-export const getLoadingStatus = (state) => state.courseUnit.loadingStatus;
 export const getSequenceStatus = (state) => state.courseUnit.sequenceStatus;
 export const getSequenceIds = (state) => state.courseUnit.courseSectionVertical.courseSequenceIds;
 export const getCourseSectionVertical = (state) => state.courseUnit.courseSectionVertical;
-export const getCourseId = state => state.courseDetail.courseId;
-export const getSequenceId = state => state.courseUnit.sequenceId;
-export const getCourseVerticalChildren = state => state.courseUnit.courseVerticalChildren;
-export const getClipboardData = state => state.courseUnit.clipboardData;
+export const getCourseId = (state) => state.courseDetail.courseId;
+export const getSequenceId = (state) => state.courseUnit.sequenceId;
+export const getCourseVerticalChildren = (state) => state.courseUnit.courseVerticalChildren;
+export const getClipboardData = (state) => state.courseUnit.clipboardData;
+const getLoadingStatuses = (state) => state.courseUnit.loadingStatus;
+export const getIsLoading = createSelector(
+  [getLoadingStatuses],
+  loadingStatus => Object.values(loadingStatus)
+    .some((status) => status === RequestStatus.IN_PROGRESS),
+);
