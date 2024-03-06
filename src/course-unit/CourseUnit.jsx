c6eadbc2b406321728b9fc31de53d40679870cb4
ruzniaievdm <ruzniaievdm@gmail.com>
2024-03-06 13:44:56 +0100

    feat: [AXIMST-538] Add errors handling 4xx, 5xx (#196)
    * feat: [AXIMST-538] Add errors handling 4xx, 5xx
    
    * fix: resolve discussions
    
    * fix: second round of review discussions

--- src/course-unit/CourseUnit.jsx
+++ src/course-unit/CourseUnit.jsx
@@ -4,9 +4,9 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Container, Layout, Stack } from '@openedx/paragon';
 import { useIntl, injectIntl } from '@edx/frontend-platform/i18n';
-import { DraggableList, ErrorAlert } from '@edx/frontend-lib-content-components';
 import { Warning as WarningIcon } from '@openedx/paragon/icons';
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { DraggableList } from '@edx/frontend-lib-content-components';
 
 import { getProcessingNotification } from '../generic/processing-notification/data/selectors';
 import SubHeader from '../generic/sub-header/SubHeader';
@@ -15,7 +15,7 @@ import getPageHeadTitle from '../generic/utils';
 import AlertMessage from '../generic/alert-message';
 import { PasteComponent } from '../generic/clipboard';
 import ProcessingNotification from '../generic/processing-notification';
-import InternetConnectionAlert from '../generic/internet-connection-alert';
+import { SavingErrorAlert } from '../generic/saving-error-alert';
 import ConnectionErrorAlert from '../generic/ConnectionErrorAlert';
 import Loading from '../generic/Loading';
 import AddComponent from './add-component/AddComponent';
@@ -39,14 +39,12 @@ const CourseUnit = ({ courseId }) => {
     isLoading,
     sequenceId,
     unitTitle,
-    isQueryPending,
+    errorMessage,
     sequenceStatus,
     savingStatus,
     isTitleEditFormOpen,
-    isErrorAlert,
     staticFileNotices,
     currentlyVisibleToStudents,
-    isInternetConnectionAlertFailed,
     unitXBlockActions,
     sharedClipboardData,
     showPasteXBlock,
@@ -54,7 +52,6 @@ const CourseUnit = ({ courseId }) => {
     handleTitleEditSubmit,
     headerNavigationsActions,
     handleTitleEdit,
-    handleInternetConnectionFailed,
     handleCreateNewCourseXBlock,
     handleConfigureSubmit,
     courseVerticalChildren,
@@ -100,9 +97,6 @@ const CourseUnit = ({ courseId }) => {
     <>
       <Container size="xl" className="course-unit px-4">
         <section className="course-unit-container mb-4 mt-5">
-          <ErrorAlert hideHeading isError={savingStatus === RequestStatus.FAILED && isErrorAlert}>
-            {intl.formatMessage(messages.alertFailedGeneric, { actionName: 'save', type: 'changes' })}
-          </ErrorAlert>
           <SubHeader
             hideBorder
             title={(
@@ -217,13 +211,10 @@ const CourseUnit = ({ courseId }) => {
           isShow={isShowProcessingNotification}
           title={processingNotificationTitle}
         />
-        {isQueryPending && (
-          <InternetConnectionAlert
-            isFailed={isInternetConnectionAlertFailed}
-            isQueryPending={savingStatus === RequestStatus.PENDING}
-            onInternetConnectionFailed={handleInternetConnectionFailed}
-          />
-        )}
+        <SavingErrorAlert
+          savingStatus={savingStatus}
+          errorMessage={errorMessage}
+        />
       </div>
     </>
   );
