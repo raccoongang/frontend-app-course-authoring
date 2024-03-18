4fe542a7b0885affd0f9b2c6772dfde2a406b97d
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2024-03-18 15:50:01 +0200

    fix: [AXIMST-656] Course unit - Fixed space between xblocks (#206)
    * fix: [AXIMST-656] fixed space between xblocks
    
    * refactor: styles for copy-paste alerts

--- src/course-unit/CourseUnit.jsx
+++ src/course-unit/CourseUnit.jsx
@@ -132,6 +132,7 @@ const CourseUnit = ({ courseId }) => {
             <Layout.Element>
               {currentlyVisibleToStudents && (
                 <AlertMessage
+                  className="course-unit__alert"
                   title={intl.formatMessage(messages.alertUnpublishedVersion)}
                   variant="warning"
                   icon={WarningIcon}
@@ -143,7 +144,7 @@ const CourseUnit = ({ courseId }) => {
                   courseId={courseId}
                 />
               )}
-              <Stack gap={4} className="mb-4 course-unit__xblocks">
+              <Stack className="mb-4 course-unit__xblocks">
                 <DraggableList
                   itemList={unitXBlocks}
                   setState={setUnitXBlocks}
@@ -163,6 +164,7 @@ const CourseUnit = ({ courseId }) => {
                       unitXBlockActions={unitXBlockActions}
                       handleConfigureSubmit={handleConfigureSubmit}
                       data-testid="course-xblock"
+                      className="course-unit__xblock"
                       userPartitionInfo={userPartitionInfo}
                     />
                   ))}
