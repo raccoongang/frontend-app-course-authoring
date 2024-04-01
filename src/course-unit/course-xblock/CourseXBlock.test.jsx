f4c0741378d19c250260a67ba688685c11582936
monteri <lansevermore>
2024-04-01 12:54:05 +0200

    fix: remove mock

--- src/course-unit/course-xblock/CourseXBlock.test.jsx
+++ src/course-unit/course-xblock/CourseXBlock.test.jsx
@@ -43,13 +43,6 @@ const unitXBlockActionsMock = {
   handleDuplicate: handleDuplicateMock,
 };
 
-jest.mock('../../content-tags-drawer/data/apiHooks', () => ({
-  useContentTaxonomyTagsCount: jest.fn(() => ({
-    isSuccess: true,
-    data: 17,
-  })),
-}));
-
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate,
