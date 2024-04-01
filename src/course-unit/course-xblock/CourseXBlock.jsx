f2b231ab3d0668bd33e748e07a4a71ca1a915115
monteri <36768631+monteri@users.noreply.github.com>
2024-04-01 13:07:36 +0200

    fix: tags count after rebase (#219)
    * fix: tags count after rebase
    
    * fix: remove mock
    
    ---------
    
    Co-authored-by: monteri <lansevermore>

--- src/course-unit/course-xblock/CourseXBlock.jsx
+++ src/course-unit/course-xblock/CourseXBlock.jsx
@@ -12,7 +12,7 @@ import { find } from 'lodash';
 import { getConfig } from '@edx/frontend-platform';
 
 import ContentTagsDrawer from '../../content-tags-drawer/ContentTagsDrawer';
-import { useContentTaxonomyTagsCount } from '../../content-tags-drawer/data/apiHooks';
+import { useContentTagsCount } from '../../generic/data/apiHooks';
 import TagCount from '../../generic/tag-count';
 import DeleteModal from '../../generic/delete-modal/DeleteModal';
 import ConfigureModal from '../../generic/configure-modal/ConfigureModal';
@@ -52,7 +52,7 @@ const CourseXBlock = ({
   const {
     data: contentTaxonomyTagsCount,
     isSuccess: isContentTaxonomyTagsCountLoaded,
-  } = useContentTaxonomyTagsCount(id || '');
+  } = useContentTagsCount(id || '');
 
   const [searchParams] = useSearchParams();
   const locatorId = searchParams.get('show');
